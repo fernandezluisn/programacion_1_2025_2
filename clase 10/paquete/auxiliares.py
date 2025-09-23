85442b6a60f7926038a6cd9b3de0fc1c9c2f5a82
Luis Fernandez <fernandezluisn@gmail.com>
2025-09-23 11:53:17 -0300

    correccion slicing

--- clase 10/paquete/auxiliares.py
+++ clase 10/paquete/auxiliares.py
@@ -8,7 +8,13 @@ def recortar_lista(lista: list,
         if lista[i] == limite:
             indice_final = i
             break
+    
+    #No es valido en parcial
+    #lista_retorno = lista[0: indice_final]
 
-    lista_retorno = lista[0: indice_final]
+    lista_retorno = [0] * (indice_final)
+
+    for i in range(len(lista_retorno)):
+        lista_retorno[i] = lista[i]
 
     return lista_retorno
